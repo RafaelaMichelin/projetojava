d2ecca42bcb79c75cd37154b3240205a4cb62d9c
RafaelaMichelin <Rafaelamichelin@gmail.com>
2025-02-24 09:49:21 -0300

    ex01 java

---
+++ ex01.java
@@ -0,0 +1,18 @@
+public class ex01
+{
+public static void main(String[] args)
+{
+    int var1 = 0;
+    int var2 = 1;
+    int var3 = 2;
+    
+    int soma = var1 + var2 + var3;
+//calcular a média
+double media = (soma/3);
+
+
+System.out.println(" A soma dos numeros e: " + soma);
+System.out.println("A media dos numeros e: " + media);
+
+}
+}
---
+++ ex02/ex02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ex02 {
+
+public static void main(String[] arg) 
+{
+    Scanner scanner = new Scanner (System.in);
+
+System.out.printf("Digite o comprimento de piscina (m):");
+double comprimento = scanner.nextDouble();
+System.out.printf("Digite a largura (m):");
+double largura = scanner.nextDouble();
+
+}
+}
